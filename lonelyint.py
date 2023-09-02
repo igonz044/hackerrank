e4b4ed070695ec53e5c155e5ccefd8a4fc164c83
Itzel G <itzelg@Itzels-MacBook-Air.local>
2023-09-02 06:24:32 -0700

    lonelyint hackerank

---
+++ lonelyint.py
@@ -0,0 +1,22 @@
+import math
+import os
+import random
+import re
+import sys
+
+def find_lonelyint(int_array):
+    for i in range(len(int_array)):
+        if int_array.count(int_array[i]) == 1:
+            lonely_int = int_array[i]
+
+    return lonely_int
+
+if __name__ == '__main__':
+    print("int_arr: ")
+    int_array = input().strip().split()
+    int_array = list(map(int,int_array))
+
+
+    print("Solution: ")
+    result = find_lonelyint(int_array)
+    print(result)
