9b48ea8e18e8dc45a2744ae867a6ef4370634dd4
igonz044 <igonz044@ucr.edu>
2024-04-12 13:45:23 -0700

    another april coding problem

---
+++ 04-longestSubstring.js
@@ -0,0 +1,32 @@
+function longestSubstring(A){
+    //we will save longest substring as B, then return its length
+    let B = [];
+    let maxB = [];
+
+    for(let i = 0; i < A.length; i++){
+
+        if(!B.includes(A[i])){
+            B.push(A[i]);
+            if(B.length>maxB.length){
+                maxB = B;
+            }
+        }
+        else{
+            i--;
+            B=[];
+        }
+    }
+    return maxB.length;
+}
+
+//Test Cases
+let A = "ab"; //output: 2
+let B = "bbbb";//output: 1
+let C = "pwwkew";//output: 3
+
+console.log(longestSubstring(B));
+
+// let C = A[0];
+// let D = B.includes(A[0]);
+// console.log(D);
+//a,b,c,d,a,a,b,b,c,d,e,g,t,b,b,b,a,a
