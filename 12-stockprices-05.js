7a6b4e1b12dd5625c580f016657b1bfd31453dea
igonz044 <igonz044@ucr.edu>
2024-06-14 17:03:14 -0700

    stock price problem solved

--- 12-stockprices-05.js
+++ 12-stockprices-05.js
@@ -8,7 +8,22 @@ What if the price goes down all day? In that case, the best profit will be negat
 
 You can do this in O(n) time and O(1) space!
 */
-const stockPrices = [10, 7, 5, 8, 11, 9];
+//const stockPrices = [10, 7, 5, 8, 11, 9];
+const stockPrices = [10, 9, 8, 7, 6];
 
-getMaxProfit(stockPrices);
+//Brute Force: push all calculations onto a list and find max
+function getMaxProfit(stockPrices){
+    let max = 0;
+    let priceCalculations = [];
+
+    for(let i = 0; i < stockPrices.length; i++){
+        for(let j = i+1; j < stockPrices.length; j++){
+            priceCalculations.push(stockPrices[j]-stockPrices[i]);
+        }
+    }
+    
+    //return priceCalculations;
+    return Math.max(...priceCalculations);
+}
+console.log(getMaxProfit(stockPrices));
 // Returns 6 (buying for $5 and selling for $11)
